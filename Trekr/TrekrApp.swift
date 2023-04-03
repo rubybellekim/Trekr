aa9c41e0e45516e6d16b22181735b0c7f06463d7
Ruby Kim <rubybellekim@gmail.com>
2023-04-02 17:42:49 -0700

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Ruby Kim on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
